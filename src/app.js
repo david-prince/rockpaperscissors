f692aaff4b962505d0e05c761face48bd01ed151
cambell-prince <cambell.prince@gmail.com>
2014-05-03 22:05:14 +1200

    Fix whoWins function and call it from buttonClick

--- src/app.js
+++ src/app.js
@@ -11,16 +11,46 @@ angular.module('david.playground', [])
 		  } else {
 			  comChoice = "scissors";
 		  }
-		  return comChoice;
+		  return comChoice;		  
 	  };
+	  
+	  var whoWins = function(choice1,choice2){
+		  var result = "";
+		  if (choice1 === choice2) {
+			  result = "You tied with the computer!";
+		  } else if (choice1 === "rock") {
+			  if (choice2 === "scissors") {
+				  result = "rock wins!"; 
+			  } else {
+				  result = "paper wins!";
+			  };
+		  } else if (choice1 === "paper") {
+			  if (choice2 === "rock"){
+				  result = "paper wins!";
+			  } else {
+				  result = "scissors wins!";
+			  };
+		  } else if (choice1 === "scissors") {
+			  if (choice2 === "rock") {
+				  result = "rock wins!";
+			  } else {
+				  result = "scissors wins!";
+			  };
+		  }
+		  return result;
+	  }; 
+
 	  $scope.message = "Choose rock, paper, or scissors. Select your choice below.";
 	  $scope.choice = "";
 	  
 	  $scope.buttonClick = function() {
-		  var bob = computerChoice();
-		  console.log(bob);
+		  var computer = computerChoice();
+		  var human = $scope.choice;
 		  
-		  $scope.message = "";
+		  var result = whoWins(human, computer); 
+		  console.log(computer, human, result);
+		  
+		  $scope.message = result;
 		  
 	  };
 	  
